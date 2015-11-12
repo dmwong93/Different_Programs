c110177403522e774d1c04c254d6d66116a384c9
dmwong93 <dmwong56@yahoo.com>
2015-11-12 13:48:37 -0800

    The class has been finished

--- File_Array_Test.java
+++ File_Array_Test.java
@@ -10,19 +10,26 @@ public class File_Array_Test
 
 	Scanner kb = new Scanner(System.in);
 
-	System.out.print("Enter the amount of values entering: ");
+	System.out.print("How many values are you entering: ");
 	values = kb.nextInt();
 
 	arrayList = new double[values];
 	for (int index = 0; index < arrayList.length; index++)	
 	{
-		System.out.print("Enter the values for number " +
+		System.out.print("Enter the value for number " +
 			          (index + 1) + ": ");
 		arrayList[index] = kb.nextDouble();
 	}
 			
 	File_Array numberDisplay = new File_Array(arrayList);
 
-	System.out.println("This average of you numbers is: " + numberDisplay.average());
+	System.out.println("Some facts about your numbers:");
+	System.out.println("-------------");
+	System.out.println("This is the sum of all your numbers: " + numberDisplay.sum());	
+	System.out.println("This is the average of your numbers: " + numberDisplay.average());
+	System.out.println("This is the highest of your numbers: " + numberDisplay.highestValue());
+	System.out.println("This is the lowest of your numbers: " + numberDisplay.lowestValue());
+	
+
 	}
 }
