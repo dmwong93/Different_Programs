b32f72aa8ab76ed50944d0eac6add9748a2c64c9
dmwong93 <dmwong56@yahoo.com>
2015-11-14 11:29:46 -0800

    Done

--- Constructors_Calc.java
+++ Constructors_Calc.java
@@ -1,10 +1,11 @@
 import javax.swing.JOptionPane;
 import javax.swing.*;
 import javax.swing.ImageIcon;
+import java.io.*;
 
 public class Constructors_Calc
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		String task;
 		final ImageIcon icon = new ImageIcon("/Users/derekwong/Desktop/1.jpeg");
@@ -16,30 +17,56 @@ public class Constructors_Calc
 					"2. Convert feet to meters\n" +
 					"3. Convert inches to centemeters\n" +
 					"4. Convert inches to feet\n" +
-					"5. Convert temperature\n" +
+					"5. Convert Fº to Cº\n" +
 					"6. Display Client Bugget\n" +
 					"Which of these task would you like to perform(enter " +
 					"only the number.)");
 		int result = Integer.parseInt(task);
 
-		switch (result)
-		{
-			case 1:
-			double area;
-			area = displayArea();
-			JOptionPane.showMessageDialog(null, "The area is: " + area);
-			break;
+			switch (result)
+			{
+				case 1:
+				double area;
+				area = displayArea();
+				JOptionPane.showMessageDialog(null, "The area is: " + area);
+				break;
 
-			case 2:
-			double meters;
-			meters = displayMeter();
-			JOptionPane.showMessageDialog(null, "Meters: " + meters);
-			break;
+				case 2:
+				double meters;
+				meters = displayMeter();
+				JOptionPane.showMessageDialog(null, "Meters: " + meters);
+				break;
 			
-			default:
-			System.out.print("yolo");
+				case 3:
+				double cent;
+				cent = displayCent();
+				JOptionPane.showMessageDialog(null, "Centermeters: " + cent);
+				break;
 
-		}
+				case 4:
+				double fee;
+				fee = displayFeet();
+				JOptionPane.showMessageDialog(null, "Feet: " + fee);
+				break;
+			
+				case 5:
+				double cel;
+				cel = displayC();
+				JOptionPane.showMessageDialog(null, "Celsius: " + cel);
+				break;
+			
+				case 6:
+				displayBud();
+				JOptionPane.showMessageDialog(null, "Your Budget Cost was saved to your computer.");
+				
+				break;
+
+				default:
+				JOptionPane.showMessageDialog(null, "You entered an invalid number, try again");
+				
+			}
+		
+		
 	}
 	public static double displayArea()
 	{
@@ -70,7 +97,56 @@ public class Constructors_Calc
 			return meter = feet / 3.28084; 
 	}
 	
+	public static double displayCent()
+	{
+			double inches;
+			double cent;
+			String inche;
+			inche = JOptionPane.showInputDialog("How many inches");
+			
+			inches = Double.parseDouble(inche);
+		
+			return cent = inches * 2.54;
+	}
 
+	public static double displayFeet()
+	{
+			double inche;
+			double feet;
+			String inches;
+			inches = JOptionPane.showInputDialog("How many inches");
+			
+			inche = Double.parseDouble(inches);
+
+			return feet = inche * 12;
+	}
+
+	public static double displayC()
+	{
+
+			double fah;
+			double c;
+			String f;
+			f = JOptionPane.showInputDialog("What's the Fahrenheit: ");
+			
+			fah = Double.parseDouble(f);
 	
+			return c = (fah - 32) * 5/9;
+	}
+
+	public static void displayBud() throws IOException
+	{
+			String material = JOptionPane.showInputDialog("Material Cost: ");
+			String labor = JOptionPane.showInputDialog("Labor Cost: ");
+			String time = JOptionPane.showInputDialog("Time on Job: ");
+
+			PrintWriter outputFile = new PrintWriter("Client_Budget.txt");
+			outputFile.println(material);
+			outputFile.println(labor);
+			outputFile.println(time);
+			outputFile.close();
+	}			
+
+
 }
 						
